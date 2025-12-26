e4d3aa050e8b6af9b92681fca10be6b1ba0c0e0a
Elham-Barzegar <elhambarzegar.hs@gmail.com>
2025-12-26 13:08:21 +0330

    fill the main_Menu from the Api

--- src/components/layouts/header/menu/Menu.tsx
+++ src/components/layouts/header/menu/Menu.tsx
@@ -1,30 +1,57 @@
 import {IconBox} from "@/components";
 
-import{browsCategoriesMock} from "@/mock/browsCategory";
+import {browsCategoriesMock} from "@/mock/browsCategory";
 import {menuMock} from "@/mock/menu";
 import Link from "next/link";
 import {useQuery} from "@tanstack/react-query";
 import {getMenuApiCall} from "@/api/Menu";
+import {EntityType, MenuItemType, MenuType, PopulateType} from "@/types";
 
 export function Menu() {
     //TODO load menu data from api
 
-    const {data: menuData} = useQuery({queryKey:[getMenuApiCall.name], queryFn: () => getMenuApiCall()})
+    const {data: menuData} = useQuery({queryKey: [getMenuApiCall.name], queryFn: () => getMenuApiCall()})
     console.log(menuData)
+
+    let mainMenuItems: null | PopulateType<MenuItemType> = null;
+    if (menuData) {
+        const findMenu = menuData.data.filter((item: EntityType<MenuType>) => item.attributes.position === 'main_menu')
+        // console.log(findMenu);
+        if (findMenu.length > 0) {
+            mainMenuItems = findMenu[0].attributes.menu_items;
+            mainMenuItems.data.sort((a:EntityType<MenuItemType>, b: EntityType<MenuItemType>)=>{
+                if (a.attributes.rank < b.attributes.rank)
+                    return -1;
+                if (a.attributes.rank > b.attributes.rank)
+                    return 1;
+                return 0;
+            })
+        }
+    }
+
     return (
         <>
-            <div id="all_categories" className="flex relative cursor-pointer bg-green-200 gap-2.5 text-white px-4 py-3 rounded-[5px] items-center">
+            <div id="all_categories"
+                 className="flex relative cursor-pointer bg-green-200 gap-2.5 text-white px-4 py-3 rounded-[5px] items-center">
 
-                <IconBox icon={'icon-apps'} size={24} title={'Browse All Categories'} link={'#'} titleClassName={'text-medium'}/>
+                <IconBox icon={'icon-apps'} size={24} title={'Browse All Categories'} link={'#'}
+                         titleClassName={'text-medium'}/>
                 <IconBox icon={'icon-angle-small-down'} size={24}/>
-                <div id="all_categories_box" className="hidden absolute z-20 bg-white left-0 top-16 w-[500px] rounded-[5px] border-[1px] border-green-300 p-[30px] hover:cursor-default">
+                <div id="all_categories_box"
+                     className="hidden absolute z-20 bg-white left-0 top-16 w-[500px] rounded-[5px] border-[1px] border-green-300 p-[30px] hover:cursor-default">
                     <div id="all_cat_inner_box" className="flex flex-wrap justify-between gap-y-[15px]">
 
-                        <IconBox link={'#'} icon={'icon-groceries-1'} size={30} title={'Milks and Dairies'} titleClassName={"text-heading-sm text-blue-300"} linkClassName={"gap-3.5 rounded-[5px] lg:border-[1px] lg:border-gray-300 py-2.5 basis-[calc(50%-8px)] justify-start pl-4 lg:hover:border-green-300"} path={5}/>
+                        <IconBox link={'#'} icon={'icon-groceries-1'} size={30} title={'Milks and Dairies'}
+                                 titleClassName={"text-heading-sm text-blue-300"}
+                                 linkClassName={"gap-3.5 rounded-[5px] lg:border-[1px] lg:border-gray-300 py-2.5 basis-[calc(50%-8px)] justify-start pl-4 lg:hover:border-green-300"}
+                                 path={5}/>
 
                         {
-                            browsCategoriesMock.map((item,index) => {
-                                return <IconBox key={index} link={item.link} icon={item.icon} size={30} title={item.title} titleClassName={"text-heading-sm text-blue-300"} linkClassName={"gap-3.5 rounded-[5px] lg:border-[1px] lg:border-gray-300 py-2.5 basis-[calc(50%-8px)] justify-start pl-4 lg:hover:border-green-300"} path={item.iconPath}/>
+                            browsCategoriesMock.map((item, index) => {
+                                return <IconBox key={index} link={item.link} icon={item.icon} size={30}
+                                                title={item.title} titleClassName={"text-heading-sm text-blue-300"}
+                                                linkClassName={"gap-3.5 rounded-[5px] lg:border-[1px] lg:border-gray-300 py-2.5 basis-[calc(50%-8px)] justify-start pl-4 lg:hover:border-green-300"}
+                                                path={item.iconPath}/>
                             })
                         }
 
@@ -42,20 +69,39 @@ export function Menu() {
                 <ul className="flex flex-col lg:flex-row items-start lg:items-center text-heading6 lg:text-heading-sm 2xl:text-heading6 gap-[32px] mt-[32px] lg:mt-0 lg:gap-3 xl:gap-5 2xl:gap-10">
 
                     {
-                        menuMock.map((item, index) => {
-
+                        mainMenuItems &&
+                        mainMenuItems.data.map((item: EntityType<MenuItemType>, index: number) => {
                             return (
-                            <li>
-                                {
-                                    item.icon ?
-                                        <IconBox {...item} size={24}/>
-                                        : <Link href={item.link} className="flex flex-row gap-2 items-center">{item.title}</Link>
+
+                                <li key={index}>
+                                    {
+
+                                        item.attributes.icon_name ?
+                                            <IconBox link={item.attributes.link} icon={item.attributes.icon_name} title={item.attributes.title} size={24}/>
+                                            : <Link href={item.attributes.link} className="flex flex-row gap-2 items-center">{item.attributes.title}</Link>
 
                                     }
-                                </li>
+                               </li>
                         )
-                    })
+                        })
                     }
+
+
+                    {/*{*/}
+                    {/*    menuMock.map((item, index) => {*/}
+
+                    {/*        return (*/}
+                    {/*        <li>*/}
+                    {/*            {*/}
+                    {/*                item.icon ?*/}
+                    {/*                    <IconBox {...item} size={24}/>*/}
+                    {/*                    : <Link href={item.link} className="flex flex-row gap-2 items-center">{item.title}</Link>*/}
+
+                    {/*                }*/}
+                    {/*            </li>*/}
+                    {/*    )*/}
+                    {/*})*/}
+                    {/*}*/}
 
                 </ul>
             </nav>
---
+++ src/types/api/Menu.ts
@@ -0,0 +1,8 @@
+import {MenuItemType, PopulateType} from "@/types";
+
+export interface MenuType {
+    title: string
+    position: string
+    menu_items : PopulateType<MenuItemType>
+
+}
