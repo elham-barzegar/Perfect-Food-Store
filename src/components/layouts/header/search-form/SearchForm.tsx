a7462d30d2349b1c04d578d9c54aea99f740d586
Elham-Barzegar <elhambarzegar.hs@gmail.com>
2025-12-30 12:54:15 +0330

    implement live search and debounce using closure

--- src/components/layouts/header/search-form/SearchForm.tsx
+++ src/components/layouts/header/search-form/SearchForm.tsx
@@ -1,15 +1,93 @@
 import {IconBox} from "@/components";
+import {useForm} from "react-hook-form";
+import {useMutation} from "@tanstack/react-query";
+import {getAllProductsApiCall} from "@/api/config/Product";
+import {useEffect, useState} from "react";
+import {EntityType} from "@/types";
+import {ProductType} from "@/types/api/product";
+import useDebounce from "@/hooks/use-debounce";
 
-interface props{
-    inputClassName? : string;
+interface props {
+    inputClassName?: string;
 }
-export function SearchForm({inputClassName = ''} : props) {
+
+interface FormInput {
+    search_text: string;
+}
+
+interface FilterData {
+    title: { $containsi: string }
+}
+
+export function SearchForm({inputClassName = ''}: props) {
     // TODO should implement form
+    const [resultData, setResultData] = useState<Array<EntityType<ProductType>>>([]);
+
+    const {register, handleSubmit, watch} = useForm<FormInput>();
+
+    const mutation = useMutation({mutationFn: (data: FilterData) => getAllProductsApiCall({filters: data})})
+    const search_text = watch('search_text');
+
+
+
+
+
+
+// *************** Add live search    ***************
+    useEffect(() => {
+        if (search_text) {
+            delay()
+        } else {
+            setResultData([])
+        }
+    }, [search_text]);
+
+    const onSubmit = (data: FormInput) => {
+        if (data.search_text.length <= 1) {
+            return;
+        }
+        mutation.mutate({
+            title: {
+                '$containsi': data.search_text
+            }
+        }, {
+            onSuccess: (response) => {
+                setResultData(response.data)
+            }
+
+        })
+
+    }
+
+// *************** Add Debounce delay using Closure    ***************
+    const delay = useDebounce(handleSubmit(onSubmit), 1000)
+
+
+
     return (
-            <form name="search-form" action="#" method="post" className="flex items-center">
-                <input type="text" name="search_text" placeholder="Search for items" className={`text-xsmall text-gray-400 border-gray-300 w-full focus:outline-none ${inputClassName}`} />
+        <div className={"relative"}>
+            <form name="search-form" onSubmit={handleSubmit(onSubmit)} action="#" method="post"
+                  className="flex items-center">
+                <input autoComplete={"off"} type="text" {...register('search_text')} placeholder="Search for items"
+                       className={`text-xsmall text-gray-400 border-gray-300 w-full focus:outline-none ${inputClassName}`}/>
                 <button type="submit"><IconBox icon={"icon-search"} size={22}/></button>
             </form>
+
+            {
+                resultData &&
+                <div className={"absolute bg-white w-full left-0 right-0 top-14"}>
+                    <ul>
+                        {
+                            resultData.map((item: EntityType<ProductType>, index) => {
+                                return <li
+                                    className={"p-4 hover:bg-green-200 hover:text-white cursor-pointer"}>{item.attributes.title}</li>
+                            })
+                        }
+                    </ul>
+                </div>
+            }
+        </div>
+
     );
 }
 
---
+++ src/hooks/use-debounce.ts
@@ -0,0 +1,13 @@
+
+// *************** Add Debounce delay using Closure    ***************
+import {useState} from "react";
+
+export default function useDebounce(func: Function, delay: number) {
+
+    const[timer,setTimer] = useState<number>()
+    return function () {
+        clearTimeout(timer);
+        let timerTimeout = setTimeout(func, delay);
+        setTimer(timerTimeout)
+    }
+}
